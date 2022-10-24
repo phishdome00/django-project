075317dca1c588ece483ce8e488d386c903ab748
puneet <puneet76.sharma@gmail.com>
2022-10-25 03:29:30 +0530

    changes in apps module

--- apps/views.py
+++ apps/views.py
@@ -1,4 +1,3 @@
-from functools import partial
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.permissions import AllowAny
@@ -15,67 +14,44 @@ from .serializers import AppSerializer,AppListSerializer
 class AppCreateUpdateView(APIView):
 
     def post(self,request,format=None):
+        user=request.user
+        data=request.data
 
-        user=request.user.is_superuser
-        if user:
-            data={}
-            username=request.data.get('username', None)
-            user_fk=User.objects.get(username=username).id
-            data['user']=user_fk
-            data['appname']=request.data.get('appname')
-            data['appurl']=request.data.get('appurl')
-            serializer = AppSerializer(data=data)
-            if serializer.is_valid():
-                serializer.save()
-                return Response(serializer.data, status=status.HTTP_201_CREATED)
-            else:
-                return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        # set to mutable
+        data._mutable = True
+
+        data['user']=user.pk
+
+        serializer = AppSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
         else:
-            return Response({"message":"permission error"}, status=status.HTTP_401_UNAUTHORIZED)
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
         
     def patch(self, request, pk, format=None):
-
-        user=request.user.is_superuser
-        if user:
-            data={}
-            username=request.data.get('username')
-            if username:
-                user_fk=User.objects.get(username=username).id
-                data['user']=user_fk
-            elif request.data.get('appname'):    
-                data['appname']=request.data.get('appname')
-            elif request.data.get('appurl'):
-                data['appurl']=request.data.get('appurl')
-
-            obj= App.objects.get(id=pk)
-            serializer = AppSerializer(data=data, instance=obj, partial=True)
-            if serializer.is_valid():
-                serializer.save()
-                return Response(serializer.data, status=status.HTTP_201_CREATED)
-            else:
-                return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        data=request.data
+        obj= App.objects.get(id=pk)
+        serializer = AppSerializer(data=data, instance=obj, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
         else:
-            return Response({"message":"permission error"}, status=status.HTTP_401_UNAUTHORIZED)
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
 
     def delete(self, request, pk, format=None):
-        
-        user=request.user.is_superuser
-        if user:
-            obj=App.objects.get(id=pk)
-            obj.delete()
-            return Response({"message":"App Data deleted Successfully !!!!"}, status=status.HTTP_200_OK)
+        obj=App.objects.get(id=pk)
+        obj.delete()
+        return Response({"message":"App Data deleted Successfully !!!!"}, status=status.HTTP_200_OK)
 
 class AppListView(APIView):
 
-    def post(self,request,format=None):
-        user=request.user.is_superuser
-        if user:
-            data={}
-            username=request.data.get('username', None)
-            user_fk=User.objects.get(username=username).id
-            queryset = App.objects.filter(user=user_fk)
-            serializer = AppListSerializer(queryset, many=True)
-            return Response({"apps":serializer.data}, status=status.HTTP_200_OK)
-        else:
-            return Response({"message":"permission error"}, status=status.HTTP_401_UNAUTHORIZED)
+    def get(self,request,format=None):
+        user=request.user
+        queryset = App.objects.filter(user=user)
+        serializer = AppListSerializer(queryset, many=True)
+        return Response({"apps":serializer.data}, status=status.HTTP_200_OK)
+
 
