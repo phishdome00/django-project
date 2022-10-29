22d3e4be0b826067c8d86327aa349de79f180964
puneet <puneet76.sharma@gmail.com>
2022-10-29 15:32:48 +0530

    api changes

--- violations/admin.py
+++ violations/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Violation
 
 # Register your models here.
+@admin.register(Violation)
+class AppAdmin(admin.ModelAdmin):
+    list_display = ('id', 'url', 'title', 'app', 'date')
+    list_display_links = ('id', 'url', 'title', 'app')
+
